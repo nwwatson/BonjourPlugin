305e6d0b46a57986ce295ef4fd9391de626373b8
Nicholas W. Watson <nicholas.w.watson@me.com>
2015-08-25 23:05:20 -0400

    small marriage between BonjourPlugin and ZeroConf

--- src/android/BonjourPlugin.java
+++ src/android/BonjourPlugin.java
@@ -1,25 +1,319 @@
 package com.nwwsolutions.plugins;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
 
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
-import org.json.JSONObject;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
+import org.apache.cordova.PluginResult.Status;
+import org.apache.http.conn.util.InetAddressUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.net.wifi.WifiManager;
+import android.util.Log;
+
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceEvent;
+import javax.jmdns.ServiceInfo;
+import javax.jmdns.ServiceListener;
 
 public class BonjourPlugin extends CordovaPlugin {
+	WifiManager.MulticastLock lock;
+	private JmDNS jmdns = null;
+	private ServiceListener listener;
+	private CallbackContext callback;
 
-  public static final String ACTION_START_SERVICE_DISCOVERY = "startServiceDiscovery";
+	@Override
+	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+		super.initialize(cordova, webView);
 
-  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-    if (ACTION_START_SERVICE_DISCOVERY.equals(action)){
-      String serviceType = args.getString(0);
-      String searchDomain = args.getString(1);
+		WifiManager wifi = (WifiManager) this.cordova.getActivity()
+				.getSystemService(android.content.Context.WIFI_SERVICE);
+		lock = wifi.createMulticastLock("BonjourPluginLock");
+		lock.setReferenceCounted(true);
+		lock.acquire();
 
-      callbackContext.success(serviceType + searchDomain);
+		Log.v("BonjourPlugin", "Initialized");
+	}
+
+	@Override
+	public boolean execute(String action, JSONArray args,
+			CallbackContext callbackContext) {
+		this.callback = callbackContext;
+
+		if (action.equals("startServiceDiscovery")) {
+			final String type = args.optString(0) + "." + args.optString(1) + ".";
+			if (type != null) {
+				cordova.getThreadPool().execute(new Runnable() {
+					public void run() {
+						watch(type); // Thread-safe.
+					}
+				});
+			} else {
+				callbackContext.error("Service type not specified.");
+				return false;
+			}
+		} else if (action.equals("unwatch")) {
+			final String type = args.optString(0);
+			if (type != null) {
+				cordova.getThreadPool().execute(new Runnable() {
+					public void run() {
+						unwatch(type);
+					}
+				});
+			} else {
+				callbackContext.error("Service type not specified.");
+				return false;
+			}
+		} else if (action.equals("register")) {
+			JSONObject obj = args.optJSONObject(0);
+			if (obj != null) {
+				final String type = obj.optString("type");
+				final String name = obj.optString("name");
+				final int port = obj.optInt("port");
+				final String text = obj.optString("text");
+				if (type == null) {
+					callbackContext.error("Missing required service info.");
+					return false;
+				}
+				cordova.getThreadPool().execute(new Runnable() {
+					public void run() {
+						register(type, name, port, text);
+					}
+				});
+			} else {
+				callbackContext.error("Missing required service info.");
+				return false;
+
+			}
+
+		} else if (action.equals("stopServiceDiscovery")) {
+			if (jmdns != null) {
+				try {
+					jmdns.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		} else if (action.equals("unregister")) {
+			if (jmdns != null) {
+				jmdns.unregisterAllServices();
+			}
+
+        } else if (action.equals("list")) {
+            final String type = args.optString(0);
+            final int timeout = args.optInt(1);
+            if (type != null && timeout > 0) {
+                cordova.getThreadPool().execute(new Runnable() {
+                    public void run() {
+                        list(type, timeout); // Thread-safe.
+                    }
+                });
+            } else {
+                callbackContext.error("Missing required parameter: type, timeout.");
+                return false;
+
+            }
+		} else {
+			Log.e("BonjourPlugin", "Invalid action: " + action);
+			callbackContext.error("Invalid action.");
+			return false;
+		}
+		PluginResult result = new PluginResult(Status.NO_RESULT);
+		result.setKeepCallback(true);
+		// return result;
+		return true;
+	}
+
+	private void watch(String type) {
+		if (jmdns == null) {
+			setupWatcher();
+		}
+		Log.d("BonjourPlugin", "Watch " + type);
+		Log.d("BonjourPlugin",
+				"Name: " + jmdns.getName() + " host: " + jmdns.getHostName());
+		jmdns.addServiceListener(type, listener);
+	}
+
+	private void unwatch(String type) {
+		if (jmdns == null) {
+			return;
+		}
+		jmdns.removeServiceListener(type, listener);
+	}
+
+	private void register(String type, String name, int port, String text) {
+		if (name == null) {
+			name = "";
+		}
+
+		if (text == null) {
+			text = "";
+		}
+
+		try {
+			ServiceInfo service = ServiceInfo.create(type, name, port, text);
+			jmdns.registerService(service);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+    private void list(String type, int timeout) {
+        try {
+            JmDNS mdnsQuery = JmDNS.create(BonjourPlugin.getIPAddress());
+            ServiceInfo[] services = mdnsQuery.list(type, timeout);
+            sendListCallback("list", services);
+            mdnsQuery.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+	private void setupWatcher() {
+		Log.d("BonjourPlugin", "Setup watcher");
+		try {
+			jmdns = JmDNS.create(BonjourPlugin.getIPAddress());
+			listener = new ServiceListener() {
+
+				public void serviceResolved(ServiceEvent ev) {
+					Log.d("BonjourPlugin", "Resolved");
+
+					sendCallback("added", ev.getInfo());
+				}
+
+				public void serviceRemoved(ServiceEvent ev) {
+					Log.d("BonjourPlugin", "Removed");
+
+					sendCallback("removed", ev.getInfo());
+				}
+
+				public void serviceAdded(ServiceEvent event) {
+					Log.d("BonjourPlugin", "Added");
+
+					// Force serviceResolved to be called again
+					jmdns.requestServiceInfo(event.getType(), event.getName(), 1);
+				}
+			};
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+	}
+
+	public void sendCallback(String action, ServiceInfo info) {
+		JSONObject status = new JSONObject();
+		try {
+			JSONArray urls = new JSONArray();
+			String[] url = info.getURLs();
+			for (int i = 0; i < url.length; i++) {
+				urls.put(url[i]);
+			}
+
+			PluginResult result = new PluginResult(PluginResult.Status.OK,
+					urls.getString(0));
+			result.setKeepCallback(true);
+			this.callback.sendPluginResult(result);
+
+		} catch (JSONException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+    public void sendListCallback(String action, ServiceInfo[] services) {
+        JSONObject status = new JSONObject();
+        try {
+            status.put("action", action);
+        	JSONArray array = new JSONArray();
+        	for (ServiceInfo service : services) {
+        	    array.put(jsonifyService(service));
+    	    }
+	        status.put("service", array);
+            Log.d("BonjourPlugin", "Sending result: " + status.toString());
+
+            PluginResult result = new PluginResult(PluginResult.Status.OK, status);
+            result.setKeepCallback(true);
+            this.callback.sendPluginResult(result);
+
+        } catch (JSONException e) {
+
+            e.printStackTrace();
+        }
 
     }
-    return true;
-  }
+
+	public static JSONObject jsonifyService(ServiceInfo info) {
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("application", info.getApplication());
+			obj.put("domain", info.getDomain());
+			obj.put("port", info.getPort());
+			obj.put("name", info.getName());
+			obj.put("server", info.getServer());
+			obj.put("description", info.getNiceTextString());
+			obj.put("protocol", info.getProtocol());
+			obj.put("qualifiedname", info.getQualifiedName());
+			obj.put("type", info.getType());
+
+			JSONArray addresses = new JSONArray();
+			String[] add = info.getHostAddresses();
+			for (int i = 0; i < add.length; i++) {
+				addresses.put(add[i]);
+			}
+			obj.put("addresses", addresses);
+			JSONArray urls = new JSONArray();
+
+			String[] url = info.getURLs();
+			for (int i = 0; i < url.length; i++) {
+				urls.put(url[i]);
+			}
+			obj.put("urls", urls);
+
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		return obj;
+
+	}
+
+	/**
+	 * Returns the first found IP4 address.
+	 *
+	 * @return the first found IP4 address
+	 */
+	public static InetAddress getIPAddress() {
+		try {
+			List<NetworkInterface> interfaces = Collections
+					.list(NetworkInterface.getNetworkInterfaces());
+			for (NetworkInterface intf : interfaces) {
+				List<InetAddress> addrs = Collections.list(intf
+						.getInetAddresses());
+				for (InetAddress addr : addrs) {
+					if (!addr.isLoopbackAddress()) {
+						String sAddr = addr.getHostAddress().toUpperCase();
+						if (InetAddressUtils.isIPv4Address(sAddr)) {
+							return addr;
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 }
